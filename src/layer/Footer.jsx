a757e4039e2c58fe9d922424a994d952887d0962
crayonzgrim <crayonzgrim@gmail.com>
2024-05-11 22:15:44 +0900

    🔨 Share this link click range modify

--- src/layer/Footer.jsx
+++ src/layer/Footer.jsx
@@ -9,7 +9,7 @@ export const Footer = () => {
     <div className="flex h-[88px] w-full flex-col items-center justify-center bg-[url('/bg_footer.png')]">
       <button
         onClick={handleCopyLink}
-        className="flex items-center justify-center flex-col gap-1 mt-2"
+        className="flex items-center justify-center flex-col gap-1 mt-2 w-full h-full"
       >
         <img src={"/share-url.png"} alt={"share-url"} width={20} />
         <p className="font-lato font-semibold text-gray-600">Share this link</p>
